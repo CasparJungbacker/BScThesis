8db3e76e195db3aaae30e236d40131f4b8a93e20
CasparJungbacker <caspar.jungbacker@outlook.com>
2021-05-21 18:33:48 +0200

    Added clipping function

--- format_data.py
+++ format_data.py
@@ -2,23 +2,30 @@ import xarray as xr
 import numpy as np
 import fnmatch
 import os
+import geopandas
+import rioxarray
+from shapely.geometry import mapping
 
 data_dir = os.path.join("..", "Data")
+
+def file_num(x):
+    return x[-5:]
 
 def preprocess(dim):
     
     global data_dir
-    
-    files = []
 
     if dim not in ["uxg", "uyg", "uzg", "rho", "sal"]:
         raise OSError(f"File {dim} not found") 
+
+    file_list = os.listdir(data_dir)
+    nc_files = []
     
-    for file in os.listdir(data_dir):
+    for file in sorted(file_list, key=file_num):
         if fnmatch.fnmatch(file, f"{dim}_part*.nc"):
-            files.append(os.path.join(data_dir, str(file)))
+            nc_files.append(os.path.join(data_dir, str(file)))
             
-    ds = xr.open_mfdataset(files, combine="nested", concat_dim="dim_2")
+    ds = xr.open_mfdataset(nc_files, combine="nested", concat_dim="dim_2")
     
     # Spatial coordinates
     latlow = 51.75
@@ -41,3 +48,17 @@ def preprocess(dim):
     da = da.rename({"dim_0": "lat", "dim_1": "lon", "dim_2": "time", "dim_3": "depth"})
     
     return da
+
+def clip(dataarray):
+
+    global data_dir
+
+    da = dataarray.rio.set_spatial_dims(x_dim="lon", y_dim="lat", inplace=True)
+
+    da.rio.write_crs("epsg:4668", inplace=True)
+
+    shape = geopandas.read_file(data_dir + "/Shapefiles/ne_10m_land.shp")
+
+    clipped = da.rio.clip(shape.geometry.apply(mapping), shape.crs, drop=False, invert=True)
+
+    return clipped
