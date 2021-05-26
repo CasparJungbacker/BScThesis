dcd1881a96e89ca0c64e6c57ac57b909382eea0b
CasparJungbacker <caspar.jungbacker@outlook.com>
2021-05-26 19:31:41 +0200

    Added simulations

--- format_data.py
+++ format_data.py
@@ -6,59 +6,58 @@ import geopandas
 import rioxarray
 from shapely.geometry import mapping
 
-data_dir = os.path.join("..", "Data")
+class data_dir:
+    def __init__(self, data_dir_path):
+        self.data_dir = data_dir_path
 
-def file_num(x):
-    return x[-5:]
 
-def preprocess(dim):
-    
-    global data_dir
+    def preprocess(self, dim):
 
-    if dim not in ["uxg", "uyg", "uzg", "rho", "sal"]:
-        raise OSError(f"File {dim} not found") 
+        if dim not in ["uxg", "uyg", "uzg", "rho", "sal"]:
+            raise OSError(f"File {dim} not found") 
 
-    file_list = os.listdir(data_dir)
-    nc_files = []
-    
-    for file in sorted(file_list, key=file_num):
-        if fnmatch.fnmatch(file, f"{dim}_part*.nc"):
-            nc_files.append(os.path.join(data_dir, str(file)))
-            
-    ds = xr.open_mfdataset(nc_files, combine="nested", concat_dim="dim_2")
-    
-    # Spatial coordinates
-    latlow = 51.75
-    latup = 53
-    lonlow = 3
-    lonup = 4.75
+        def file_num(x):
+            return x[-5:]
 
-    res = (1/(60/0.125));
-    a = np.arange(lonlow+(res*1.5)/2,lonup,res*1.5) # Longitude 560 values
-    b = np.arange(latlow+(res)/2,latup,res) # Latitude 600 values
-    
-    # Timestamps
-    time = np.load("simulation_timestamps.npy")
+        file_list = os.listdir(self.data_dir)
+        nc_files = []
 
-    # Depths
-    depths = np.arange(19, -1, -1) # meters below surface
-    
-    da = ds.assign_coords({"dim_0": b, "dim_1": a, "dim_2": time, "dim_3": depths}).transpose('dim_2', 'dim_0', 'dim_1', 'dim_3')
-    
-    da = da.rename({"dim_0": "lat", "dim_1": "lon", "dim_2": "time", "dim_3": "depth"})
-    
-    return da
+        for file in sorted(file_list, key=file_num):
+            if fnmatch.fnmatch(file, f"{dim}_part*.nc"):
+                nc_files.append(os.path.join(self.data_dir, str(file)))
 
-def clip(dataarray):
+        ds = xr.open_mfdataset(nc_files, combine="nested", concat_dim="dim_2")
 
-    global data_dir
+        # Spatial coordinates
+        latlow = 51.75
+        latup = 53
+        lonlow = 3
+        lonup = 4.75
 
-    da = dataarray.rio.set_spatial_dims(x_dim="lon", y_dim="lat", inplace=True)
+        res = (1/(60/0.125));
+        a = np.arange(lonlow+(res*1.5)/2,lonup,res*1.5) # Longitude 560 values
+        b = np.arange(latlow+(res)/2,latup,res) # Latitude 600 values
 
-    da.rio.write_crs("epsg:4668", inplace=True)
+        # Timestamps
+        time = np.load(os.path.join(self.data_dir, "simulation_timestamps.npy"))
 
-    shape = geopandas.read_file(data_dir + "/Shapefiles/ne_10m_land.shp")
+        # Depths
+        depths = np.arange(19, -1, -1) # meters below surface
 
-    clipped = da.rio.clip(shape.geometry.apply(mapping), shape.crs, drop=False, invert=True)
+        da = ds.assign_coords({"dim_0": b, "dim_1": a, "dim_2": time, "dim_3": depths}).transpose('dim_2', 'dim_3', 'dim_0', 'dim_1')
 
-    return clipped
+        da = da.rename({"dim_0": "lat", "dim_1": "lon", "dim_2": "time", "dim_3": "depth"})
+
+        return da
+
+    def clip(self, dataarray):
+
+        da = dataarray.rio.set_spatial_dims(x_dim="lon", y_dim="lat", inplace=True)
+
+        da.rio.write_crs("epsg:4668", inplace=True)
+
+        shape = geopandas.read_file(self.data_dir + "/Shapefiles/ne_10m_land.shp")
+
+        clipped = da.rio.clip(shape.geometry.apply(mapping), shape.crs, drop=True, invert=True)
+
+        return clipped
